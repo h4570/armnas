bf181473e12895144953b23352848274c3a24567
h4570 <hastoklai@gmail.com>
2021-02-20 22:52:32 +0100

    added deploy script

---
+++ deploy.ps1
@@ -0,0 +1,43 @@
+# ===--- Variables ---===
+
+$TARGET = "root@192.168.0.155"
+
+# ---
+# You can remove passwords from these commands, via installing your ssh key on target machine.
+# -> cat id_rsa.pub | ssh root@$TARGET_IP 'mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys'
+$SSH = 'plink.exe'
+$SSH_DEFAULT_ARGS = '-batch', '-pw', 'raspberry'
+$SCP = 'pscp.exe'
+$SCP_DEFAULT_ARGS = '-r', '-P', '22', '-pw', 'raspberry'
+# ----
+
+# ===--- Deploy frontend ---===
+
+Write-Host "Building Angular (frontend)" -ForegroundColor DarkGreen
+Set-Location frontend/web-app
+ng build --configuration=production --output-hashing=all --output-path=./dist > $null
+Set-Location ..
+Set-Location ..
+
+Write-Host "Deploying Angular (frontend)" -ForegroundColor Magenta
+& $SSH $SSH_DEFAULT_ARGS $TARGET, 'rm -rf /root/armnas/frontend/web-app/*' > $null
+& $SCP $SCP_DEFAULT_ARGS './frontend/web-app/dist/\*', $TARGET':/root/armnas/frontend/web-app' > $null
+
+# ===--- Deploy backend ---===
+
+Write-Host "Building Web API (backend)" -ForegroundColor DarkGreen
+Set-Location backend/WebApi
+$GIT_TAG = git describe;
+$GIT_TAG = $GIT_TAG.Substring(1)
+dotnet publish -c Release /p:version=${GIT_TAG} --output "./publish" > $null
+Set-Location ..
+Set-Location ..
+
+Write-Host "Deploying Web API (backend)" -ForegroundColor Magenta
+& $SSH $SSH_DEFAULT_ARGS $TARGET, 'rm -rf /root/armnas/backend/WebApi/*' > $null
+& $SCP $SCP_DEFAULT_ARGS './backend/WebApi/publish/\*', $TARGET':/root/armnas/backend/WebApi' > $null
+Remove-Item -LiteralPath "./backend/WebApi/publish" -Force -Recurse 
+
+# ===--- Finish ---===
+
+Write-Host "Done!" -ForegroundColor DarkGreen

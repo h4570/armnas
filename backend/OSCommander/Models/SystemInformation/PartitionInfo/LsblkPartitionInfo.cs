61bf7c216cd679fe821a1f0118103e6dc7a61f3c
h4570 <sandro.sobczynski@gmail.com>
2022-02-13 00:49:55 +0100

    fix(SystemInformation): added /boot partitions to system partitions

--- backend/OSCommander/Models/SystemInformation/PartitionInfo/LsblkPartitionInfo.cs
+++ backend/OSCommander/Models/SystemInformation/PartitionInfo/LsblkPartitionInfo.cs
@@ -5,7 +5,7 @@
         public string DeviceName => $"/dev/{Name}";
         public string Name { get; set; }
         public string MountingPoint { get; set; }
-        public bool IsMain => MountingPoint != null && MountingPoint.Equals("/");
+        public bool IsMain => MountingPoint != null && (MountingPoint.Equals("/") || MountingPoint.StartsWith("/boot"));
         public int MemoryInMB { get; set; }
         public string Uuid { get; set; }
     }
